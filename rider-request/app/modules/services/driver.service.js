f6057055770f8c3bccb91708e98384a92ecc664d
khantparthkumara <parthkhant1@gmail.com>
2022-07-25 01:19:59 +0530

    Small improvements

---
+++ rider-request/app/modules/services/driver.service.js
@@ -0,0 +1,19 @@
+import models from '../model/index';
+import { success, ExistsError } from "iyasunday";
+
+export async function getDriver(id) {
+    try {
+        const driver = await models.Driver.findOne({ _id: id }).populate("product_id");
+        if (!driver) {
+            throw new ExistsError(`${id} driver not Exist`);
+        }
+        return {
+            success,
+            message: `You have successfully get your driver`,
+            data: driver,
+        };
+    } catch (err) {
+        throw err;
+    }
+}
+
